3a0a807c340d2599815e5d08d46694aa6ffd4197
xgf <newbox0512@yahoo.com>
2015-07-13 19:18:43 +0800

    Initial Commit

---
+++ iOS Social API/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Social API
+//
+//  Created by 峰哥哥-.- on 15/7/13.
+//  Copyright (c) 2015年 峰哥哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
